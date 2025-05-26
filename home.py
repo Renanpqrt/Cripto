7bc4d7d8eadda077b926d5c02a441d34e28e40dd
Renanpqrt <renanoliveiradossantos00@hotmail.com>
2025-05-25 21:41:11 -0300

    ajustes visuais

--- home.py
+++ home.py
@@ -19,14 +19,26 @@ def abrir_home(frame_atual, janela):
     titulo = ctk.CTkLabel(frame_atual, text='Cotações', font=('Helvetica', 25, 'bold'), text_color="#ffffff", bg_color='#00d147')
     titulo.place(relx=0.5, rely=0.02, anchor='center')
 
-    b_pi = ctk.CTkButton(frame_atual, text='Pi Network', width=80, command=lambda: abrir_pi(frame_atual, janela), corner_radius=12, bg_color='#05bf44')
+    pi_img = Image.open(resource_path("imagens/pi.png"))
+    pi_img = CTkImage(light_image=pi_img, size=(30, 30))
+
+    b_pi = ctk.CTkButton(frame_atual, text='', image=pi_img, width=80, command=lambda: abrir_pi(frame_atual, janela), bg_color='#05bf44', fg_color='#05bf44', hover_color='#05bf44')
     b_pi.place(relx=0.08, rely=0.3, anchor='center')
 
-    b_btc= ctk.CTkButton(frame_atual, text='Bitcoin', width=80, command=lambda: abrir_btc(frame_atual, janela), corner_radius=12, bg_color='#05bf44')
+    btc_img = Image.open(resource_path("imagens/bitcoin.png"))
+    btc_img = CTkImage(light_image=btc_img, size=(30, 30))
+
+    b_btc= ctk.CTkButton(frame_atual, text='', image=btc_img, width=80, command=lambda: abrir_btc(frame_atual, janela), bg_color='#05bf44', fg_color='#05bf44', hover_color='#05bf44')
     b_btc.place(relx=0.08, rely=0.4, anchor='center')
 
-    b_dolar = ctk.CTkButton(frame_atual, text='Dolar', width=80, command=lambda: abrir_dolar(frame_atual, janela), corner_radius=12, bg_color='#00d147')
+    dolar_img = Image.open(resource_path("imagens/dolar.png"))
+    dolar_img = CTkImage(light_image=dolar_img, size=(30, 30))
+
+    b_dolar = ctk.CTkButton(frame_atual, text='', image=dolar_img, width=80, command=lambda: abrir_dolar(frame_atual, janela), bg_color='#00d147', fg_color="#00d147", hover_color="#00d147")
     b_dolar.place(relx=0.92, rely=0.3, anchor='center')
 
-    b_euro = ctk.CTkButton(frame_atual, text='Euro', width=80, command=lambda: abrir_euro(frame_atual, janela), corner_radius=12, bg_color='#00d147')
+    euro_img = Image.open(resource_path("imagens/euro.png"))
+    euro_img = CTkImage(light_image=euro_img, size=(30, 30))
+
+    b_euro = ctk.CTkButton(frame_atual, text='', image=euro_img, width=80, command=lambda: abrir_euro(frame_atual, janela), bg_color='#00d147', fg_color="#00d147", hover_color="#00d147")
     b_euro.place(relx=0.92, rely=0.4, anchor='center')
